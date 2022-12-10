46e477adc27e6d5bf1b36ffdd653e0e61f1b487d
Metehan <114250887+MetehanSaman@users.noreply.github.com>
2022-12-10 18:14:00 +0300

    Add files via upload

---
+++ Matlab_LN1.m
@@ -0,0 +1,48 @@
+xx=2;
+yy=1;
+xx + yy
+xx - yy
+xx * yy
+xx / yy
+
+% %= işareti yorum satırı başlangıcı belirtir.
+%clc = clear comment tab / sadece comment penceresini temizler.
+%clear all = clear workspace and comment windows / Workspace ve comment
+%pencelerini temizler.
+
+sqrt(16)
+% sqrt = karkök, sin = radyan cinsinden sinüs, sind = derece cinsinden
+% sinüs, cos,cosd,tan,tand,cot,cotd, log = logaritma, exp = üstel fonksiyon
+% anlamına gelir.
+
+x = 5;
+d = sqrt(x)
+a=1;
+b=1;
+c=1;
+Kok1=(-b+sqrt(b^2-4*a*c))/2*a
+
+x=5;
+z = sin(5*x)^2
+u = (sin(x)^2)+(cos(x)^2)
+
+Deger=input('Yaşınızı Girin:');
+
+% input = kullanıcıdan veri almak için kullanılır.
+% input('Girilmesini İstediğniz Veri ya da Soruyu buraya yazınız')
+
+
+A=Deger^2;
+C=sin(A)
+Isim = 'Ali';
+B=12;
+fprintf('%s nin yaşı %d dir \n',Isim,B)
+
+% disp() içerisine girilen değişkenin değerini gösterir disp(değişken)
+% şeklinde kullanılır.
+
+
+
+
+
+
